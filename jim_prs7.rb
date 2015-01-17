371510573391199303d31e8206bfe12b637a3fe5
James M Reinach <jmrcounseling@gmail.com>
2015-01-17 12:13:41 -0500

    Update jim_prs7.rb

--- jim_prs7.rb
+++ jim_prs7.rb
@@ -5,31 +5,29 @@ puts "Welcome to Paper, Rock, Scissors!"
 def display_winning_message(winning_choice)
   case winning_choice
     when "p"
-    puts "Paper wraps Rock!"
+      puts "Paper wraps Rock!"
     when "r"
-    puts "Rock smashes Scissors!"
+      puts "Rock smashes Scissors!"
     when "s"
-    puts "Scissors cuts Paper!"
+      puts "Scissors cuts Paper!"
   end
 end
-
 loop do
-  begin
-    puts "Choose one (p, r, s):"
-    player_choice = gets.chomp.downcase
-  end until CHOICES.keys.include?(player_choice)
-  computer_choice = CHOICES.keys.sample
+    begin
+      puts "Choose one (p, r, s):"
+      player_choice = gets.chomp.downcase
+    end until CHOICES.keys.include?(player_choice)
+    computer_choice = CHOICES.keys.sample
 
-  if player_choice == computer_choice  
-    puts "It's a tie!"
-  elsif player_choice == "p" && computer_choice == "r" || player_choice == "r" && computer_choice == "s" || player_choice == "s" && computer_choice == "p"
+    if player_choice == computer_choice  
+      puts "It's a tie!"
+    elsif player_choice == "p" && computer_choice == "r" || player_choice == "r" && computer_choice == "s" || player_choice == "s" && computer_choice == "p"
+      display_winning_message(player_choice)
     puts "You won!"
-  else
-    puts "Computer won!"
+    else
+      display_winning_message(computer_choice)
+      puts "Computer won!"
   end
-
-
   puts "Play again (y/n)"
-break if gets.chomp.downcase != "y"
-    
+  break if gets.chomp.downcase != "y"
 end
