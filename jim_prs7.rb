be95002850c0587222b77e00bdf095182f546caa
James M Reinach <jmrcounseling@gmail.com>
2015-01-17 13:22:41 -0500

    Update jim_prs7.rb

--- jim_prs7.rb
+++ jim_prs7.rb
@@ -13,20 +13,20 @@ def display_winning_message(winning_choice)
   end
 end
 loop do
-    begin
-      puts "Choose one (p, r, s):"
-      player_choice = gets.chomp.downcase
-    end until CHOICES.keys.include?(player_choice)
-    computer_choice = CHOICES.keys.sample
+ begin
+  puts "Choose one (p, r, s):"
+  layer_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
+  computer_choice = CHOICES.keys.sample
 
-    if player_choice == computer_choice  
-      puts "It's a tie!"
-    elsif player_choice == "p" && computer_choice == "r" || player_choice == "r" && computer_choice == "s" || player_choice == "s" && computer_choice == "p"
-      display_winning_message(player_choice)
+  if player_choice == computer_choice  
+    puts "It's a tie!"
+  elsif player_choice == "p" && computer_choice == "r" || player_choice == "r" && computer_choice == "s" || player_choice == "s" && computer_choice == "p"
+    display_winning_message(player_choice)
     puts "You won!"
-    else
-      display_winning_message(computer_choice)
-      puts "Computer won!"
+  else
+     display_winning_message(computer_choice)
+     puts "Computer won!"
   end
   puts "Play again (y/n)"
   break if gets.chomp.downcase != "y"
